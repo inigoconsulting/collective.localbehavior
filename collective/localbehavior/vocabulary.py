f641f55e9fa9728d4ad08ae43c6ff211eea7df40
Izhar Firdaus <izhar@inigo-tech.com>
2011-12-26 14:42:16 +0800

    added heck to query for fti when adding content

--- collective/localbehavior/vocabulary.py
+++ collective/localbehavior/vocabulary.py
@@ -1,15 +1,28 @@
 from zope.interface import directlyProvides
-from zope.component import getUtilitiesFor, getUtility
+from zope.component import getUtilitiesFor, getUtility, queryUtility
 
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 from plone.dexterity.interfaces import IDexterityFTI
 
 from plone.behavior.interfaces import IBehavior
+from zope.globalrequest import getRequest
 
 def LocalBehaviorsVocabularyFactory(context):
     behaviors = getUtilitiesFor(IBehavior)
-    fti = getUtility(IDexterityFTI, name=context.portal_type)
+    fti = queryUtility(IDexterityFTI, name=context.portal_type)
+    if fti is None:
+        # hack to get fti when adding content
+        req = getRequest()
+        path = req.physicalPathFromURL(req.getURL())
+        if not '++add++' in path[-1]:
+            return SimpleVocabulary.fromItems([])
+        ft = path[-1].replace('++add++', '')
+        fti = getattr(context.portal_types, ft, None)
+        if fti is None:
+            return SimpleVocabulary.fromItems([])
+
+    # query behaviors, exclude the content-level behaviors
     items = [
         (reg.title, reg.interface.__identifier__) for (
         title, reg) in behaviors if (
